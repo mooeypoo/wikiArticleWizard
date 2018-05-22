0baf25b74f52d8eb3cd34a96e13bbfca82b5840c
Moriel Schottlender <moriel@gmail.com>
2018-05-22 23:15:10 +0300

    remove IIFE from waw.Config

--- src/waw.Config.js
+++ src/waw.Config.js
@@ -1,6 +1,4 @@
-( function () {
-	waw.Config = {
-		ARTICLE_REPO: mw.config.get( 'articlesandbox-article-repo' ) || '',
-		NAME_FOR_SANDBOX: mw.config.get( 'articlesandbox-article-nameforsandbox' ) || 'sandbox'
-	};
-}() );
+waw.Config = {
+	ARTICLE_REPO: mw.config.get( 'articlesandbox-article-repo' ) || '',
+	NAME_FOR_SANDBOX: mw.config.get( 'articlesandbox-article-nameforsandbox' ) || 'sandbox'
+};
